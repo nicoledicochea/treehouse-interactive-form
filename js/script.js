b93cbaa226ab1461f61acb69372959211b339377
“Nicole <dicocheanicole@gmail.com>
2023-09-21 10:46:49 -0700

    add comments

--- js/script.js
+++ js/script.js
@@ -23,6 +23,10 @@ const cvv = document.querySelector('#cvv')
 const paypal = document.querySelector('#paypal')
 const bitcoin = document.querySelector('#bitcoin')
 
+/*** 
+ * @event
+ * set default values once the DOM loads
+*/
 document.addEventListener('DOMContentLoaded', () => {
     nameInput.focus()
     otherJobInput.hidden = true
@@ -33,12 +37,20 @@ document.addEventListener('DOMContentLoaded', () => {
     bitcoin.style.display = 'none'
 })
 
+/*** 
+ * @event
+ * conditionally show 'other job role' input box 
+*/
 jobRole.addEventListener('change', (e) => {
     if(e.target.value ==='other') {
         otherJobInput.hidden = false
     }
 })
 
+/*** 
+ * @event
+ * change color options when design input changes
+*/
 designSelect.addEventListener('change', (e) => {
     colorSelect.disabled = false
     const design = e.target.value
@@ -51,6 +63,10 @@ designSelect.addEventListener('change', (e) => {
     })
 })
 
+/*** 
+ * @event
+ * update activities total cost and disable conflicting activity times when checkbox input changes
+*/
 activitiesField.addEventListener('change', (e) => {
     const activityChecked = e.target
     const dayTime = document.querySelectorAll('[data-day-and-time]')
@@ -58,7 +74,10 @@ activitiesField.addEventListener('change', (e) => {
 
     let totalCost = activitiesTotalCost.innerText.replace(regex, '$3')
     totalCost = +totalCost
-
+    /*** 
+     * @function disableConflictingActivities
+     * when e.targeet checked, disable conflicting activities
+    */
     function disableConflictingActivities() {
         dayTime.forEach(checkbox => {
             if(checkbox !== activityChecked 
@@ -68,6 +87,10 @@ activitiesField.addEventListener('change', (e) => {
             }
         })
     }
+    /*** 
+     * @function disableConflictingActivities
+     * when e.targeet unchecked, enable conflicting activities
+    */
     function enableConflictingActivities() {
         dayTime.forEach(checkbox => {
             if(checkbox !== activityChecked 
@@ -77,7 +100,6 @@ activitiesField.addEventListener('change', (e) => {
             }
         })
     }
-
     if(activityChecked.checked) {
         const cost = activityChecked.dataset.cost
         totalCost += +cost
@@ -91,18 +113,30 @@ activitiesField.addEventListener('change', (e) => {
     }
 })
 
+/*** 
+ * @event
+ * add focus class when activity checkbox focused
+*/
 activityCheckboxes.forEach(checkbox => {
     checkbox.addEventListener('focus', e => {
         e.target.parentElement.classList.add('focus')
     })
 })
 
+/*** 
+ * @event
+ * remove focus class when activity checkbox not focused
+*/
 activityCheckboxes.forEach(checkbox => {
     checkbox.addEventListener('blur', e => {
         e.target.parentElement.classList.remove('focus')
     })
 })
 
+/*** 
+ * @event
+ * change payment option when payment selection changes
+*/
 paymentSelect.addEventListener('change', (e) => {
     const paymentChoice = e.target.value
     if(paymentChoice === 'paypal') {
@@ -120,8 +154,17 @@ paymentSelect.addEventListener('change', (e) => {
     }
 })
 
+/*** 
+ * @event
+ * user validation for the form upon submit
+*/
 form.addEventListener('submit', (e) => {
-
+    /*** 
+     * @function displayHint
+     * conditionally displays hint for required sections
+     * @param {@Object} element - input field
+     * @param {@Function} validator - function for validation (ie. isValidName)
+    */
     function displayHint(element, validator) {
         if(validator) {
             element.parentElement.classList.add('valid')
@@ -134,35 +177,67 @@ form.addEventListener('submit', (e) => {
             element.nextElementSibling.style.display = 'block'
         }
     }
-
+    /*** 
+     * @function isValidName
+     * checks if the user input is not blank
+     * @returns {@boolean}
+    */
     function isValidName() {
         return nameInput.value.trim() !== ''
     }
+    /*** 
+     * @function isValidEmail
+     * checks if the user input email has the correct format
+     * @returns {@boolean}
+    */
     function isValidEmail() {
         // regex code referenced from https://emailregex.com/
         const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
         return regex.test(email.value)
     }
+    /*** 
+     * @function isValidActivity
+     * checks if at least one activity is checked
+     * @returns {@boolean}
+    */
     function isValidActivity() {
         const regex = /(\w*)(: \$)(\d{1,})/i
         let totalCost = activitiesTotalCost.innerText.replace(regex, '$3')
         totalCost = +totalCost
         return totalCost !== 0
     }
+    /*** 
+     * @function isValidCCNum
+     * checks if the credit card num has 13 - 16 digits
+     * @returns {@boolean}
+    */
     function isValidCCNum() {
         const regex = /^\d{13,16}$/
         return regex.test(ccNum.value)
     }
+    /*** 
+     * @function isValidZipCode
+     * checks if the zip code has 5 digits
+     * @returns {@boolean}
+    */
     function isValidZipCode() {
         const regex = /^\d{5}$/
         return regex.test(zip.value)
     }
+    /*** 
+     * @function isValidCVV
+     * checks if the cvv num has 3 digits
+     * @returns {@boolean}
+    */
     function isValidCVV() {
         const regex = /^\d{3}$/
         return regex.test(cvv.value)
     }
 
+    /*** 
+     * call displayHint on all required form fields
+    */
     displayHint(nameInput, isValidName())
     displayHint(email, isValidEmail())
     displayHint(activitiesTotalCost, isValidActivity())
@@ -173,6 +248,9 @@ form.addEventListener('submit', (e) => {
     }
 })
 
+/*** 
+ * create and insert the additional name input hint
+*/
 const additionalNameHint = document.createElement('span')
 additionalNameHint.innerText = "Name field cannot contain digits"
 additionalNameHint.classList.add('name-hint')
@@ -182,10 +260,22 @@ const nameLabel = nameInput.parentElement
 const nameHint = nameInput.nextElementSibling
 nameLabel.insertAdjacentElement('beforeend', additionalNameHint)
 
+/*** 
+ * @event
+ * user validation for name input when a key is released
+*/
 nameInput.addEventListener('keyup', (e) => {
+    /*** 
+     * @function isNameBlank
+     * checks if the user input is blank
+    */
     function isNameBlank() {
         return nameInput.value.trim() === ''
     }
+    /*** 
+     * @function nameContainsDigits
+     * checks if the user input has any digits
+    */
     function nameContainsDigits() {
         const regex = /\d+/
         return regex.test(nameInput.value)
